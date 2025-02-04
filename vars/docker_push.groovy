b1f6aad02fd9c39e6f28ab8c74eda2e313edb2e1
MeVikashhh <vikash.test@outlook.com>
2025-02-04 12:51:41 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,5 +2,7 @@ def call(String Project, String ImageTag, String dockerhubuser){
   withCredentials([usernamePassword(credentialsId: 'DockerHub', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
       sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
   }
+  sh "sudo usermod -aG docker $USER"
+  sh "newgrp docker"
   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
